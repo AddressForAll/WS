71efb7d84ac509b0ee3e52e021f3cd190cc4f7ae
Peter Krauss <ppkrauss@gmail.com>
2020-10-12 07:55:24 -0300

    add vw_core and clean old comments

--- src/step01-iniApi.sql
+++ src/step01-iniApi.sql
@@ -125,60 +125,73 @@ COMMENT ON FUNCTION API.uri_dispatcher_scalar
 ;
 */
 
+----------------------
 
--- -- -- -- -- -- -- --
--- API etc dispatchers:
+-- -- -- -- -- -- -- -- --
+-- API ENDPOINT FUNCTIONS:
 
 
-/* OLD, lixo.
-CREATE or replace FUNCTION API.uri_dispatch_tab_eclusa1(
-    uri text DEFAULT '', -- /eclusa/checkUserFiles-step1/{user}/{is_valid}?
-    args text DEFAULT NULL
-) RETURNS TABLE (LIKE api.ttpl_eclusa02_cityfile1) AS $f$
-  WITH topt AS (
-    SELECT CASE
-      WHEN p_len=1 AND p[1]='eclusa' THEN -2 -- incomplete
-      WHEN p_len<=1 THEN -1 -- empty
-      WHEN p[2]='checkuserfiles-step1' THEN 1
-      WHEN p[2]='checkuserfiles-step2' THEN 2
-      ELSE -3  -- not found
-      END opt,
-      CASE WHEN p_len<3 THEN NULL ELSE p[3] END AS p_user,
-      CASE WHEN p_len<4 THEN NULL ELSE text_to_boolean(p[4]) END AS p_is_valid
-    FROM (SELECT regexp_split_to_array(trim(lower(uri),'/'), '/')) t1(p),
-         LATERAL (select array_upper(p,1) as p_len) t2
-  )
-      ( -- OPT1:
-        SELECT cityname, ctype, fid, fname, is_valid, fmeta
-             -- err_msg=fmeta->>'is_valid_err'
-        FROM topt, eclusa.cityfolder_input_files('/home/'||p_user)
-        WHERE topt.opt = 1
-             AND COALESCE( is_valid=p_is_valid, true)
-        ORDER BY 1,2
-      ) UNION ALL
-      ( -- OPT2:
-        SELECT cityname, ctype, fid, fname, is_valid, fmeta
-               -- see fmeta->>'hash' and  fmeta->>'is_valid_err'
-        FROM topt, eclusa.cityfolder_input('/home/'||p_user)
-        WHERE topt.opt = 2
-              AND COALESCE( is_valid=p_is_valid, true)
-        ORDER BY 1,2
-      ) UNION ALL -- OPT<0, error message:
-        SELECT a.* from topt, api.ttpl_eclusa02_cityfile1 a
-        WHERE topt.opt < 0 AND fmeta->'dispatching_errcod' = to_jsonb(topt.opt)
+CREATE or replace FUNCTION API.uridisp_vw_core_jurisdiction(
+    p_uri text DEFAULT '', -- e.g. /vw_core/vw_core/jurisdiction/{isolabel_ext}
+    p_args text DEFAULT NULL
+) RETURNS TABLE (LIKE api.ttpl_core01_jurisdiction) AS $f$
+        SELECT t2.*
+        FROM API.uri_dispatch_parser(p_uri) t1(p) -- rev ,'{eclusa,checkuserfiles_step2}'
+        INNER JOIN optim.jurisdiction  t2 -- or api.jurisdiction?
+        ON t1.p[1] IS NOT NULL
+        WHERE CASE
+          WHEN t1.p[1] LIKE 'br-__-___' THEN
+               t2.abbrev=upper(substr(t1.p[1],7))
+               AND substr(t2.isolabel_ext,1,6)=upper(substr(t1.p[1],1,6))
+          WHEN t1.p[1] iLIKE 'br;__;%' THEN
+               t2.lexlabel=lower(substr(t1.p[1],7))
+               AND substr(t2.isolabel_ext,1,6)=replace(upper(substr(t1.p[1],1,6)),';','-')
+           WHEN t1.p[1]~'^q\d+$' THEN t2.wikidata_id=substr(t1.p[1],2)::bigint
+           WHEN t1.p[1]~'^\d+$'  THEN CASE   -- ID numerico?
+               WHEN t1.p[2] IS NOT NULL THEN -- com segundo parametro?
+                t1.p[2]~'^\d+$' AND t2.jurisd_local_id=(t1.p[2])::int AND t2.jurisd_base_id=(t1.p[1])::int
+               ELSE t2.osm_id=(t1.p[1])::bigint -- OSM relation
+               END
+           ELSE CASE   -- codigo nao-numerico no primeiro parametro.
+               WHEN t1.p[2] IS NULL THEN upper(t2.isolabel_ext)=upper(t1.p[1])
+               ELSE -- dois parametros
+                  t2.jurisd_base_id=(SELECT jurisd_base_id FROM optim.jurisdiction WHERE isolabel_ext=upper(t1.p[1]))
+                  AND
+                  t1.p[2]~'^\d+$' AND t2.jurisd_local_id=(t1.p[2])::int -- por ex. IBGE_ID
+               END
+           END
 $f$ language SQL immutable;
-COMMENT ON FUNCTION API.uri_dispatch_tab_eclusa1
-  IS 'A uri_dispatcher() returning a table, for Eclusa-step1 datatype.'
+COMMENT ON FUNCTION API.uridisp_vw_core_jurisdiction
+  IS 'Jurisdiction basic properties, from many alternatives to express its identification.'
 ;
+-- CREATE or replace FUNCTION API.uridisp_vw_core_jurisdiction_geojson() ...
+-- vai devolver o mapa com atributos basicos retornados da funcao. Decidir se havera NGINX detectando extensao.
 
----  mais lixo ainda:
-CREATE or replace FUNCTION API.uri_dispatch_tab_eclusa2(
-    uri text DEFAULT '',
-    args text DEFAULT NULL
-) RETURNS api.ttpl_eclusa03_cityfile2 AS $f$
-  -- etc
+CREATE or replace FUNCTION API.uridisp_vw_core_donor(
+    p_uri text DEFAULT '', -- /vw_core/jurisdiction/{isolabel_ext}/{geojson}
+    p_args text DEFAULT NULL
+) RETURNS TABLE (LIKE api.ttpl_core02_donor) AS $f$
+        SELECT t2.id, scope, shortname, vat_id, legalName, wikidata_id, url, info,
+             CASE
+               WHEN j1.n_packs IS NULL THEN NULL::jsonb
+               ELSE jsonb_build_object('n_files',j1.n_files, 'n_packs',j1.n_packs, 'tot_bytes',j1.tot_bytes)
+             END as kx
+        FROM API.uri_dispatch_parser(p_uri) t1(p) -- rev ,'{eclusa,checkuserfiles_step2}'
+        INNER JOIN optim.donor t2 ON t1.p[1] IS NOT NULL
+        LEFT JOIN (
+          SELECT donor_id,
+                 count(distinct pack_id) AS n_packs,
+                 count(*) AS n_files, sum((fmeta->'size')::int) as tot_bytes
+          FROM optim.vw01_origin
+          GROUP BY donor_id
+        ) j1 ON j1.donor_id = t2.id
+        WHERE CASE
+          WHEN t1.p[1]~'^\d+$'  THEN t2.id=(t1.p[1])::int
+          WHEN t1.p[1]~'^q\d+$' THEN t2.wikidata_id=(substr(t1.p[1],2)::bigint)
+          WHEN t1.p[1]~'^[a-z]+:.+$' THEN t2.kx_vat_id=optim.vat_id_normalize(t1.p[1])
+          ELSE t2.shortname=upper(t1.p[1])
+          END
 $f$ language SQL immutable;
-COMMENT ON FUNCTION API.uri_dispatch_tab_eclusa2
-  IS 'A uri_dispatcher() returning a table, for Eclusa-step2 datatype.'
+COMMENT ON FUNCTION API.uridisp_vw_core_donor
+  IS 'Donor basic properties, from many alternatives to express its identification.'
 ;
-*/
