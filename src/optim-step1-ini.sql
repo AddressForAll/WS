722b94516893e577ee93384ae835dc38bff2f3ad
cchness <cness@riseup.net>
2021-12-28 10:42:08 -0300

    Corrige comentario.

--- src/optim-step1-ini.sql
+++ src/optim-step1-ini.sql
@@ -287,7 +287,7 @@ $f$ language SQL immutable;
 -- Pre-insert (generating FDWs)
 
 \echo E'\n --- FDW para ingestão de dados do git ---'
-SELECT ingest.fdw_generate_getclone('jurisdiction', 'br', 'optim', null,null, '/tmp/pg_io/digital-preservation-XX'); -- creates tmp_orig.s
+SELECT ingest.fdw_generate_getclone('jurisdiction', 'br', 'optim', null,null, '/tmp/pg_io/digital-preservation-XX'); -- creates tmp_orig.fdw_jurisdiction_br
 
 SELECT ingest.fdw_generate(
   'donatedPack',  'br', 'optim',
